c015aa6cbcaa25ad0f08344cfaef49b1701181b7
Kevinarrasyid <kevinarrasyid3@gmail.com>
2022-02-21 15:57:02 +0700

    repo awal

---
+++ menu.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int pilih;
+
+int main(){
+	printf("\n======MENU======");
+	printf("\n1. New File");
+	printf("\n2. Open");
+	printf("\n3. Recent");
+	printf("\n4. Theme");
+	printf("\n5. Exit");
+	printf("\nPilih : ");
+	scanf("%d", &pilih);
+
+	return 0;
+}
