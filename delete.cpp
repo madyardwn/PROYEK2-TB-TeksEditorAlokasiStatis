fd52ae35a05d12bda9c7285bc4a03f4dda722c9d
aurelldhndr <46367624+aurelldhndr@users.noreply.github.com>
2022-03-12 13:06:30 +0700

    Add files via upload

---
+++ delete.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include "delete.h"
+
+void delete_rows (string mas[]) 
+{
+	int row1,row2;
+	printf("Masukkan baris yang ingin di hapus \n");
+	printf("From : ");
+	scanf("%d", &row1);
+	row1=row1-1;
+	
+	pritnf("\n To: ");
+	scanf("%d", &row2);
+	row2=row2-1;
+	
+	for ( row1 ; row1<N-row1 ; row1++) 
+	{
+		row2++;
+		mas[row1]=mas[row2];
+	}
+	ofstream file2("FF.txt",ios::in); 
+	for (int i=0 ; i<=N; i++) 
+	{
+		file2<< mas[i]<<endl;
+	}
+}
---
+++ delete.h
@@ -0,0 +1,7 @@
+#ifndef header_h
+#define header_h
+
+void delete_rows();
+void delete_file();
+
+#endif
