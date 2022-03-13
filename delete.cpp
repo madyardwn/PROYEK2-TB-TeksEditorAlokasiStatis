f59b20ec34d31b4584adde6f59fdf26aa4c09945
aurelldhndr <46367624+aurelldhndr@users.noreply.github.com>
2022-03-13 10:33:54 +0700

    Add files via upload

--- delete.cpp
+++ delete.cpp
@@ -1,32 +1,60 @@
 #include <stdio.h>
 #include <conio.h>
-#include <fstream>
-#include <string>
+#include "stdlib.h"
 #include "delete.h"
 
-void delete_rows (string mas[]) 
+void delete_rows()
 {
-	int row1,row2;
-	printf("Masukkan baris yang ingin di hapus \n");
-	printf("Dari baris : ");
-	scanf("%d", &row1);
-	row1=row1-1;
-	
-	pritnf("\n Sampai Baris : ");
-	scanf("%d", &row2);
-	row2=row2-1;
-	
-	for ( row1 ; row1<N-row1 ; row1++) 
-	{
-		row2++;
-		mas[row1]=mas[row2];
-	}
-	ofstream file2("FF.txt",ios::in); 
-	for (int i=0 ; i<=N; i++) 
-	{
-		file2<< mas[i]<<endl;
-	}
+  FILE *fptr1, *fptr2;
+  char file1[] = "file1.txt";
+  char file2[] = "file2.txt";
+  char curr;
+  int del, line_number = 0;
+  printf("Masukkan nomor baris yang akan dihapus : ");
+  scanf("%d", &del);
+  fptr1 = fopen(file1, "r");
+  fptr2 = fopen(file2, "w");
+  curr = getc(fptr1);
+  if (curr != EOF)
+  {
+    line_number = 1;
+  }
+  while (1)
+  {
+    if (del != line_number)
+      putc(curr, fptr2);
+    curr = getc(fptr1);
+    if (curr == '\n')
+      line_number++;
+    if (curr == EOF)
+      break;
+  }
+  
+  fclose(fptr1);
+  fclose(fptr2);
 }
 
+// prototype function
+int destroy_file(char nama_file_temp[30]);
+
 void delete_file(){
+    char namaFile[30];
+    bool statusFile;
+    printf("\t\tMasukkan nama file yang akan dihapus: ");
+    scanf("%s", namaFile);
+    statusFile = destroyFile(namaFile);
+    if (statusFile == true){
+        printf("File : %s berhasil dihapus\n", namaFile);
+    } else  {
+        printf("File : %s gagal dihapus atau file tidak ada di dalam direktori\n", namaFile);
+    }
 }
+
+int destroy_file(char nama_file_temp[30]) {
+    system("cls");
+    if (remove(nama_file_temp) == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
--- delete.h
+++ delete.h
@@ -1,7 +1,8 @@
-#ifndef header_H
-#define header_H
+#ifndef header_h
+#define header_h
 
 void delete_rows();
+int destroy_file(char nama_file_temp[30]);
 void delete_file();
 
 #endif
