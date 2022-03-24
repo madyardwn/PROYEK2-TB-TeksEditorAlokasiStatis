337f791b34b755cf3a510e64aca925ece2f049bc
madyardwn <madyardwn@gmail.com>
2022-03-24 09:15:43 +0700

    input level 2

---
+++ insert.h
@@ -0,0 +1,14 @@
+#ifndef insert_H
+#define insert_H
+
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+#define ROWS 20
+#define COLS 40 
+
+void gotoxy(int x, int y);
+void Insert(char arr[ROWS][COLS]);
+
+#endif
