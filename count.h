2e07b8854a27aaf606fd48007fd557737c0668ba
FahmiX <99156264+FahmiX@users.noreply.github.com>
2022-03-28 03:46:48 +0700

    Add files via upload

---
+++ count.h
@@ -0,0 +1,7 @@
+#ifndef count_h
+#define count_h
+
+int count_char(char arr[MAX_ROWS][MAX_COLUMNS]);
+void cursor_information(int baris, int kolom, char arr[MAX_ROWS][MAX_COLUMNS]);
+
+#endif
--- theme.h
+++ theme.h
@@ -1,6 +1,7 @@
 #ifndef header_h
 #define header_h
 
-void themes();
+void menu_themes();
+void custom_themes(bool *status);
 
 #endif
