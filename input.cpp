ae2742197723a324f4ec11c4c78a90070939beba
FahmiX <99156264+FahmiX@users.noreply.github.com>
2022-05-08 23:03:10 +0700

    Update input normal, enter, dan kursor
    untuk kursor arrow atas dan bawah belum sempurna

--- input.cpp
+++ input.cpp
@@ -6,7 +6,7 @@
 #include "input.h"
 #include "design.h"
 
-address Alokasi(char ch, int baris, int kolom)
+address Alokasi(char ch)
 {
 	address P;
 	P = (address) malloc (sizeof(element_list));
@@ -14,10 +14,7 @@ address Alokasi(char ch, int baris, int kolom)
 	{
 		Prev(P) = NULL;
 		Info(P) = ch;
-		Baris(P) = baris;
-		Kolom(P) = kolom;
 		Next(P) = NULL;
-		
 		return P;
 	}else{
 		return NULL;
@@ -26,9 +23,12 @@ address Alokasi(char ch, int baris, int kolom)
 
 void create_text_editor(list *L)
 {
-	Head(*L) = NULL;
-	Current(*L) = NULL;
-	Tail(*L) = NULL;
+	address P;
+	P = Alokasi(NULL);
+	
+	Head(*L) = P;
+	Current(*L) = P;
+	Tail(*L) = P;
 }
 
 void input_keyboard(list *L)
@@ -38,6 +38,12 @@ void input_keyboard(list *L)
 	int baris = 0, kolom = 0;
 	bool validasi_input;
 	address P;
+	address Q;
+	
+	gotoxy(28,0);
+	printf("Baris: %d\n", baris+1); 
+	printf("Kolom: %d", kolom+1);
+	gotoxy(baris,kolom);
 	
 	while (1)
 	{
@@ -53,29 +59,27 @@ void input_keyboard(list *L)
 		validasi_input = cek_input(ch);
 		
 		// Jika tidak perlu di handling, maka input normal
-		if (validasi_input != true and (baris < MAX_ROWS-1 or kolom < MAX_COLUMNS))
+		if (validasi_input != true)
 		{
-			// Cek Kolom
-			if (kolom == MAX_COLUMNS)
+			P = Alokasi(ch);
+			if (P != NULL)
 			{
-				kolom = 0;
-				baris = baris + 1;
+				normal_input(*(&L), P, &Q, &baris, &kolom);
+				gotoxy(28,0);
+				printf("                         \n");
+				printf("                         ");
+				gotoxy(28,0);
+				printf("Baris: %d\n", baris+1); 
+				printf("Kolom: %d", kolom+1);
+				gotoxy(baris,kolom);
+			}else{
+				Beep(1000,50);
 			}
-			gotoxy(baris,kolom);
-			
-			P = Alokasi(ch, baris, kolom);
-			normal_input(*(&L), P, &baris, &kolom);
 		}else
 		
 		// Jika perlu di handling, maka input di handle terlebih dahulu
-		if (baris < MAX_ROWS-1 or kolom < MAX_COLUMNS)
 		{
 			handling_input(*(&L), ch, &baris, &kolom);
-		}else
-		
-		// Jika sudah melebihi batas baris dan kolom, maka input dihentikan
-		{
-			Beep(1000,50);
 		}
 	}
 }
@@ -128,63 +132,50 @@ bool cek_input(char ch)
 	}		
 }
 
-void normal_input(list *L, address P, int *baris, int *kolom)
-{	
-	// Kondisi Awal Menulis
-	if (Head(*L) == NULL and Current (*L) == NULL and Tail(*L) == NULL)
+void normal_input(list *L, address P, address *Q, int *baris, int *kolom)
+{
+	// Linked List
+	if (Next(Current(*L)) != NULL)
 	{
-		// Linked List
-		Head(*L) = P;
-		Tail(*L) = P;
-		Current(*L) = P;
-		
-		// Tampil Layar
-		printf("%c", Info(Current(*L)));
-	}else
-	
-	// Menulis di awal baris dan kolom 
-	if (Current(*L) == Head(*L))
-	{
-		// Linked List
-		Next(Head(*L)) = P;
-		Prev(P) = Head(*L);
-		Current(*L) = P; 
-		if (Tail(*L) == Head(*L))
-		{
-			Tail(*L) = P;
-		}
-		
-		// Tampil Layar
-		printf("%c", Info(Current(*L)));
-	}else
-	
-	// Menulis di akhir baris dan kolom
-	if (Current(*L) == Tail(*L))
-	{
-		// Linked List
-		Next(Tail(*L)) = P;
-		Prev(P) = Tail(*L);
-		Tail(*L) = P;
-		Current(*L) = P;
-		
-		// Tampil Layar
-		printf("%c", Info(Current(*L)));
-	}else
-	
-	// Menulis di suatu baris dan kolom
-	{
-		// Linked List
 		Next(P) = Next(Current(*L));
 		Prev(Next(Current(*L))) = P;
 		Next(Current(*L)) = P;
 		Prev(P) = Current(*L);
+		Current(*L) = P; 
+	}else{
+		Next(Current(*L)) = P;
+		Prev(P) = Current(*L);
+		Tail(*L) = P;
 		Current(*L) = P;
-		
-		// Tampil Layar
-		printf("%c", Info(Current(*L)));
 	}
 	
+	// Tampil Layar
+//	system("cls");
+//	P = Next(Head(*L));
+//	
+//	while(P != NULL)
+//	{
+//		printf("%c", Info(P));
+//		if (Info(P) == NULL)
+//		{
+//			printf("\n");
+//		}
+//		P = Next(P);
+//	}
+	
+	P = Current(*L);
+	while(P != NULL)
+	{
+		if (Info(P) == NULL)
+		{
+			break;
+		}
+		printf("%c", Info(P));
+		P = Next(P);
+	}
+
 	*kolom = *kolom + 1;
+	gotoxy(*baris,*kolom);
 }
 
 void handling_input(list *L, char ch, int *baris, int *kolom)
@@ -193,25 +184,25 @@ void handling_input(list *L, char ch, int *baris, int *kolom)
 	if (ch == 8)
 	{
 		// Modul Backspace
-	}
+	}else
 	
 	// Enter
 	if (ch == 13)
 	{
-		// Modul Enter
-	}
+		enter(*(&L), ch, *(&baris), *(&kolom));
+	}else
 	
 	// Arrows
 	if (ch == -32)
 	{
 		arrows(*(&L), ch, *(&baris), *(&kolom));
-	}
+	}else
 	
 	// Tab
 	if (ch == 9)
 	{
 		// Modul Tab
-	}
+	}else
 	
 	// ESC
 	if (ch == 27)
@@ -223,6 +214,7 @@ void handling_input(list *L, char ch, int *baris, int *kolom)
 void arrows(list *L, char ch, int *baris, int *kolom)
 {	
 	address P;
+	int count = 0;
 	ch = getch();
 	
 	switch(ch)
@@ -230,38 +222,156 @@ void arrows(list *L, char ch, int *baris, int *kolom)
 		// Up Arrow
 		case 72:
 		{
-			// Jika berada pada baris paling awal
-			if (*baris == 0)
+			P = Current(*L);
+			if (P == Head(*L))
 			{
 				gotoxy(*baris,*kolom);
-			}else
+				break;
+			}
 			
-			if (*baris != 0)
+			// Hitung Posisi dari NULL
+			*kolom = 0;
+			while(Info(P) != NULL)
 			{
-				P = Current(*L);
-				for (int i = *kolom; i >= 0; i--)
+				P = Prev(P);
+				count = count + 1;
+			}
+			P = Prev(P);
+			
+			if (P == NULL)
+			{
+				*kolom = count;
+				Current(*L) = P;
+				gotoxy(*baris,*kolom);
+				break;
+			}
+			
+			if (Info(P) == NULL)
+			{
+				Current(*L) = P;
+			}else{
+				while(Info(P) != NULL)
 				{
 					P = Prev(P);
 				}
+				
+				// Ubah Posisi
+				for (int i=0; i < count; i++)
+				{
+					P = Next(P);
+					*kolom = *kolom + 1;
+					if (Info(P) == NULL)
+					{
+						Current(*L) = Prev(Current(*L));
+						*kolom = *kolom - 1;
+						break;
+					}	
+				}
+				Current(*L) = P;
 			}
+			*baris = *baris - 1;
+			gotoxy(*baris,*kolom);
 			break;
 		}
 		
 		// Down Arrow
 		case 80:
 		{
+			P = Current(*L);
+			if (P == Tail(*L))
+			{
+				gotoxy(*baris,*kolom);
+				break;
+			}
+			
+			// Hitung Posisi dari NULL
+			*kolom = 0;
+			while(Info(P) != NULL)
+			{
+				P = Prev(P);
+				count = count + 1;
+			}
+			P = Next(P);
+			
+			if (Info(P) == NULL)
+			{
+				Current(*L) = P;
+			}else{
+				while(Info(P) != NULL)
+				{
+					P = Next(P);
+				}
+				
+				// Ubah Posisi
+				for (int i=0; i < count; i++)
+				{
+					if (Next(P) != NULL)
+					{
+						P = Next(P);
+						Current(*L) = P;
+						*kolom = *kolom + 1;
+						if (Info(P) == NULL)
+						{
+							Current(*L) = Prev(Current(*L));
+							*kolom = *kolom - 1;
+							break;
+						}	
+					}else{
+						Current(*L) = P;
+						break;
+					}
+				}
+			}
+			*baris = *baris + 1;
+			gotoxy(*baris,*kolom);
+			
 			break;
 		}
 		
 		// Right Arrow
 		case 77:
 		{
+			P = Current(*L);
+			if (Next(P) != NULL)
+			{
+				Current(*L) = Next(P);
+				*kolom = *kolom + 1;
+				if (Info(Current(*L)) == NULL)
+				{
+					*kolom = 0;
+					*baris = *baris + 1;
+				}
+				gotoxy(*baris,*kolom);
+			}
 			break;
 		}
 		
 		// Left Arrow
 		case 75:
 		{
+			P = Current(*L);
+			if (Prev(P) != NULL)
+			{
+				Current(*L) = Prev(P);
+				*kolom = *kolom - 1;
+				if (Info(P) == NULL)
+				{
+					P = Prev(P);
+					while(Info(P) != NULL)
+					{
+						P = Prev(P);
+					}
+					*kolom = 0;
+					P = Next(P);
+					while(Info(P) != NULL)
+					{
+						P = Next(P);
+						*kolom = *kolom + 1;
+					}
+					*baris = *baris - 1;
+				}
+				gotoxy(*baris,*kolom);
+			}
 			break;
 		}
 		
@@ -272,21 +382,57 @@ void arrows(list *L, char ch, int *baris, int *kolom)
 	}
 }
 
+void enter(list *L, char ch, int *baris, int *kolom)
+{
+	address P;
+	
+	P = Alokasi(NULL);
+	if (Next(Current(*L)) != NULL)
+	{
+		Prev(P) = Current(*L);
+		Next(P) = Next(Current(*L));
+		Prev(Next(Current(*L))) = P;
+		Next(Current(*L)) = P;
+		Current(*L) = P;
+	}else{
+		Next(Current(*L)) = P;
+		Prev(P) = Current(*L);
+		Current(*L) = P;
+	}
+	
+	// Tampil Layar
+	system("cls");
+	P = Next(Head(*L));
+	
+	while(P != NULL)
+	{
+		printf("%c", Info(P));
+		if (Info(P) == NULL)
+		{
+			printf("\n");
+		}
+		P = Next(P);
+	}
+	*baris = *baris + 1;
+	*kolom = 0;
+	gotoxy(*baris,*kolom);
+}
+            
 void tampil_list(list L)
 {
 	system("cls");
 	printf("Hasil Tulisan\n\n");
 	
 	address P;
-	P = Head(L);
+	P = Next(Head(L));
 	
 	while(P != NULL)
 	{
 		printf("%c", Info(P));
-		P = Next(P);
-		if (Baris(P) != 0 and Kolom(P) == 0)
+		if (Info(P) == NULL)
 		{
 			printf("\n");
 		}
+		P = Next(P);
 	}
 }
--- input.h
+++ input.h
@@ -1,10 +1,12 @@
 #ifndef input_h
 #define input_h
 
+#include <stdbool.h>
+
 #define Prev(P) P->prev
 #define Info(P) P->info
-#define Baris(P) P->baris
-#define Kolom(P) P->kolom
+#define First(P) P->first
+#define Last(P) P->last
 #define Next(P) P->next
 #define Head(L) (L).head
 #define Current(L) (L).current
@@ -18,8 +20,8 @@ typedef struct element_list *address;
 typedef struct element_list{
 	address prev;
 	char info;
-	int baris;
-	int kolom;
+	bool first;
+	bool last;
 	address next;	
 }element_input;
 
@@ -29,13 +31,14 @@ typedef struct{
 	address tail;
 }list;
 
-address Alokasi(char ch, int baris, int kolom);
+address Alokasi(char ch);
 void create_text_editor(list *L);
 void input_keyboard(list *L);
 bool cek_input(char ch);
-void normal_input(list *L, address P, int *baris, int *kolom);
+void normal_input(list *L, address P, address *Q, int *baris, int *kolom);
 void handling_input(list *L, char ch, int *baris, int *kolom);
 void arrows(list *L, char ch, int *baris, int *kolom);
+void enter(list *L, char ch, int *baris, int *kolom);
 void tampil_list(list L);
 
 #endif
